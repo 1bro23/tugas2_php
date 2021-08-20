cb9b9a1c71d33af6d7c0871b8aa3cb159ed65015
1bro23 <shinlia21@gmail.com>
2021-08-20 22:18:00 +0800

    add tugas 2 php

---
+++ tugas2_1.php
@@ -0,0 +1,10 @@
+<?php
+  echo "36 hari = 12 pekerja<br>X hari = 24 pekerja<br>X = ...?<br>Jawaban :<br>";
+  $hari36=12;
+  $pekerja12=36;
+  $Xhari=24;
+  $usaha=$pekerja12*$hari36;
+  $pekerja24=$usaha/$Xhari;
+  echo "usaha = $pekerja12(jumlah hari) * $hari36(jumlah pekerja) = $usaha<br>";
+  echo "X hari = $usaha(total usaha) / $Xhari(jumlah pekerja) = $pekerja24(hari)";
+?>
---
+++ tugas2_2.php
@@ -0,0 +1,9 @@
+<?php
+  echo "Jual = 80000<br>Beli = 50000<br>Keuntungan = ?<br>Jawaban :<br>";
+  $jual=80000;
+  $beli=50000;
+  $keuntungan=(80000-50000)/50000;
+  $dalampersen=$keuntungan*100;
+  $dalampersen2="$dalampersen%";
+  echo "Keuntungan = (80000-50000)/50000 = $keuntungan atau $dalampersen2";
+?>
